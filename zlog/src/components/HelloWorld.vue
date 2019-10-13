d39e5c43676a5dd51733340bda4337c1dd3834ed
seyoun.kim <zebra0303@gmail.com>
2019-10-13 22:47:04 +0900

    the first commit

---
+++ zlog/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} :-))</h1>
+      <div class="hello">
+        <h4>아이디</h4>
+        <input v-model="email" type="text"></input>
+        <h4>패스워드</h4>
+        <input v-model="password" type="password"></input>
+        <div><button @click="signUp()">가입하기</button></div>
+      </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    signUp () {
+      this.$store.dispatch('signInAction', {email: this.email, password: this.password})
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ zlog/src/store.js
@@ -0,0 +1,72 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { vuexfireMutations, firestoreAction } from 'vuexfire';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/analytics';
+
+Vue.use(Vuex);
+
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const db = firebase.initializeApp(config).firestore();
+firebase.analytics();
+
+export default new Vuex.Store({
+  // setup the reactive todos property
+  state: {
+    user: { 
+      info: {},
+      status: null,
+      error: null
+    },
+    posts: []
+  },
+
+  mutations: {
+    setUser (state, payload) {
+      state.user = payload;
+    },
+    removeUser (state) {
+      state.user = null;
+    },
+    setStatus (state, payload) {
+      state.status = payload;
+    },
+    setError (state, payload) {
+      state.error = payload;
+    },
+    ...vuexfireMutations
+  },
+
+  actions: {
+    signInAction ({commit}, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+      .then((rep) => {
+        console.log(rep.user);
+        commit('setUser', rep.user.uid);
+        commit('setStatus', 'success');
+      })
+      .catch((error) => {
+        console.log(error.code)
+        console.log(error.message)
+      }); 
+    },
+    bindPostsRef: firestoreAction(context => {
+      // context contains all original properties like commit, state, etc
+      // and adds `bindFirestoreRef` and `unbindFirestoreRef`
+      // we return the promise returned by `bindFirestoreRef` that will
+      // resolve once data is ready
+      return context.bindFirestoreRef('posts', db.collection('posts'));
+    })
+  }
+});
