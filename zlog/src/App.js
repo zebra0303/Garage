57825fa75bac38bad0c018782e2e330ba5ca4288
Larry Kim <zebra0303@gmail.com>
2020-01-17 22:40:17 +0900

    init

--- zlog/src/App.js
+++ zlog/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.scss';
+
+import React, { useEffect, useState } from 'react';
+import getAllProducts from './api/getAllProducts';
 
 function App() {
+
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    getAllProducts.then(data =>  setData(data))    
+  }, []);
+  
   return (
     <div className="App">
-      :)
+      <pre>
+        {JSON.stringify(data, null, 1 )}
+      </pre>
     </div>
   );
 }
